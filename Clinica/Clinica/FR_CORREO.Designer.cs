387f481cb3947859acd5cadd88d3baa12bf48395
bmazariegos <bryanestuard3@gmail.com>
2020-08-20 16:45:58 -0600

    Ultimo arreglo a envio de correo

--- Clinica/Clinica/FR_CORREO.Designer.cs
+++ Clinica/Clinica/FR_CORREO.Designer.cs
@@ -34,38 +34,44 @@
             this.btn_enviar = new System.Windows.Forms.Button();
             this.lbl_para = new System.Windows.Forms.Label();
             this.lbl_asunto = new System.Windows.Forms.Label();
-            this.txt_cc = new System.Windows.Forms.TextBox();
-            this.lbl_cc = new System.Windows.Forms.Label();
             this.txt_mensaje = new System.Windows.Forms.TextBox();
             this.btn_regresar = new System.Windows.Forms.Button();
             this.lbl_titulo = new System.Windows.Forms.Label();
             this.lbl_mensaje = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_codigo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txt_para
             // 
-            this.txt_para.Location = new System.Drawing.Point(180, 135);
+            this.txt_para.Location = new System.Drawing.Point(135, 115);
+            this.txt_para.Margin = new System.Windows.Forms.Padding(2);
             this.txt_para.Name = "txt_para";
-            this.txt_para.Size = new System.Drawing.Size(355, 22);
+            this.txt_para.Size = new System.Drawing.Size(267, 20);
             this.txt_para.TabIndex = 2;
-            this.txt_para.TextChanged += new System.EventHandler(this.TX_PARA_TextChanged);
+            this.txt_para.TextChanged += new System.EventHandler(this.txt_para_TextChanged);
             // 
             // txt_asunto
             // 
-            this.txt_asunto.Location = new System.Drawing.Point(180, 274);
+            this.txt_asunto.Location = new System.Drawing.Point(135, 159);
+            this.txt_asunto.Margin = new System.Windows.Forms.Padding(2);
             this.txt_asunto.Name = "txt_asunto";
-            this.txt_asunto.Size = new System.Drawing.Size(355, 22);
+            this.txt_asunto.Size = new System.Drawing.Size(267, 20);
             this.txt_asunto.TabIndex = 3;
             // 
             // btn_enviar
             // 
             this.btn_enviar.BackColor = System.Drawing.Color.White;
             this.btn_enviar.Font = new System.Drawing.Font("Corbel", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_enviar.Location = new System.Drawing.Point(630, 154);
+            this.btn_enviar.Image = ((System.Drawing.Image)(resources.GetObject("btn_enviar.Image")));
+            this.btn_enviar.Location = new System.Drawing.Point(346, 297);
+            this.btn_enviar.Margin = new System.Windows.Forms.Padding(2);
             this.btn_enviar.Name = "btn_enviar";
-            this.btn_enviar.Size = new System.Drawing.Size(163, 48);
+            this.btn_enviar.Size = new System.Drawing.Size(74, 69);
             this.btn_enviar.TabIndex = 5;
-            this.btn_enviar.Text = "Enviar";
             this.btn_enviar.UseVisualStyleBackColor = false;
             this.btn_enviar.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -74,9 +80,10 @@
             this.lbl_para.AutoSize = true;
             this.lbl_para.BackColor = System.Drawing.Color.Transparent;
             this.lbl_para.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_para.Location = new System.Drawing.Point(48, 122);
+            this.lbl_para.Location = new System.Drawing.Point(36, 104);
+            this.lbl_para.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_para.Name = "lbl_para";
-            this.lbl_para.Size = new System.Drawing.Size(74, 37);
+            this.lbl_para.Size = new System.Drawing.Size(59, 29);
             this.lbl_para.TabIndex = 6;
             this.lbl_para.Text = "Para";
             // 
@@ -85,47 +92,32 @@
             this.lbl_asunto.AutoSize = true;
             this.lbl_asunto.BackColor = System.Drawing.Color.Transparent;
             this.lbl_asunto.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_asunto.Location = new System.Drawing.Point(48, 261);
+            this.lbl_asunto.Location = new System.Drawing.Point(36, 148);
+            this.lbl_asunto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_asunto.Name = "lbl_asunto";
-            this.lbl_asunto.Size = new System.Drawing.Size(106, 37);
+            this.lbl_asunto.Size = new System.Drawing.Size(84, 29);
             this.lbl_asunto.TabIndex = 7;
             this.lbl_asunto.Text = "Asunto";
             // 
-            // txt_cc
-            // 
-            this.txt_cc.Location = new System.Drawing.Point(180, 203);
-            this.txt_cc.Name = "txt_cc";
-            this.txt_cc.Size = new System.Drawing.Size(355, 22);
-            this.txt_cc.TabIndex = 8;
-            // 
-            // lbl_cc
-            // 
-            this.lbl_cc.AutoSize = true;
-            this.lbl_cc.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_cc.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_cc.Location = new System.Drawing.Point(48, 190);
-            this.lbl_cc.Name = "lbl_cc";
-            this.lbl_cc.Size = new System.Drawing.Size(48, 37);
-            this.lbl_cc.TabIndex = 9;
-            this.lbl_cc.Text = "Cc";
-            this.lbl_cc.Click += new System.EventHandler(this.label3_Click);
-            // 
             // txt_mensaje
             // 
-            this.txt_mensaje.Location = new System.Drawing.Point(180, 354);
+            this.txt_mensaje.Location = new System.Drawing.Point(135, 197);
+            this.txt_mensaje.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_mensaje.Multiline = true;
             this.txt_mensaje.Name = "txt_mensaje";
-            this.txt_mensaje.Size = new System.Drawing.Size(355, 22);
+            this.txt_mensaje.Size = new System.Drawing.Size(267, 96);
             this.txt_mensaje.TabIndex = 10;
             // 
             // btn_regresar
             // 
             this.btn_regresar.BackColor = System.Drawing.Color.White;
             this.btn_regresar.Font = new System.Drawing.Font("Corbel", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_regresar.Location = new System.Drawing.Point(630, 261);
+            this.btn_regresar.Image = ((System.Drawing.Image)(resources.GetObject("btn_regresar.Image")));
+            this.btn_regresar.Location = new System.Drawing.Point(637, 1);
+            this.btn_regresar.Margin = new System.Windows.Forms.Padding(2);
             this.btn_regresar.Name = "btn_regresar";
-            this.btn_regresar.Size = new System.Drawing.Size(163, 48);
+            this.btn_regresar.Size = new System.Drawing.Size(38, 39);
             this.btn_regresar.TabIndex = 11;
-            this.btn_regresar.Text = "Regresar";
             this.btn_regresar.UseVisualStyleBackColor = false;
             this.btn_regresar.Click += new System.EventHandler(this.button1_Click_1);
             // 
@@ -134,9 +126,10 @@
             this.lbl_titulo.AutoSize = true;
             this.lbl_titulo.BackColor = System.Drawing.Color.Transparent;
             this.lbl_titulo.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_titulo.Location = new System.Drawing.Point(173, 28);
+            this.lbl_titulo.Location = new System.Drawing.Point(130, 23);
+            this.lbl_titulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_titulo.Name = "lbl_titulo";
-            this.lbl_titulo.Size = new System.Drawing.Size(271, 37);
+            this.lbl_titulo.Size = new System.Drawing.Size(216, 29);
             this.lbl_titulo.TabIndex = 12;
             this.lbl_titulo.Text = "ENVIO DE CORREO ";
             // 
@@ -145,34 +138,97 @@
             this.lbl_mensaje.AutoSize = true;
             this.lbl_mensaje.BackColor = System.Drawing.Color.Transparent;
             this.lbl_mensaje.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_mensaje.Location = new System.Drawing.Point(48, 341);
+            this.lbl_mensaje.Location = new System.Drawing.Point(36, 186);
+            this.lbl_mensaje.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_mensaje.Name = "lbl_mensaje";
-            this.lbl_mensaje.Size = new System.Drawing.Size(122, 37);
+            this.lbl_mensaje.Size = new System.Drawing.Size(98, 29);
             this.lbl_mensaje.TabIndex = 13;
             this.lbl_mensaje.Text = "Mensaje";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Corbel", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(244, 297);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 71);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btn_adjuntar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 370);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Adjuntar PDF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(344, 368);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 29);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Enviar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 67);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 29);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Código";
+            // 
+            // txt_codigo
+            // 
+            this.txt_codigo.Location = new System.Drawing.Point(135, 78);
+            this.txt_codigo.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_codigo.Name = "txt_codigo";
+            this.txt_codigo.Size = new System.Drawing.Size(168, 20);
+            this.txt_codigo.TabIndex = 17;
+            this.txt_codigo.TextChanged += new System.EventHandler(this.txt_codigo_TextChanged_1);
+            this.txt_codigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_codigo_KeyPress);
+            // 
             // FR_CORREO
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(900, 550);
+            this.ClientSize = new System.Drawing.Size(675, 447);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_codigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_mensaje);
             this.Controls.Add(this.lbl_titulo);
             this.Controls.Add(this.btn_regresar);
             this.Controls.Add(this.txt_mensaje);
-            this.Controls.Add(this.lbl_cc);
-            this.Controls.Add(this.txt_cc);
             this.Controls.Add(this.lbl_asunto);
             this.Controls.Add(this.lbl_para);
             this.Controls.Add(this.btn_enviar);
             this.Controls.Add(this.txt_asunto);
             this.Controls.Add(this.txt_para);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FR_CORREO";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FR_CORREO";
-            this.Load += new System.EventHandler(this.FR_CORREO_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,11 +240,14 @@
         private System.Windows.Forms.Button btn_enviar;
         private System.Windows.Forms.Label lbl_para;
         private System.Windows.Forms.Label lbl_asunto;
-        private System.Windows.Forms.TextBox txt_cc;
-        private System.Windows.Forms.Label lbl_cc;
         private System.Windows.Forms.TextBox txt_mensaje;
         private System.Windows.Forms.Button btn_regresar;
         private System.Windows.Forms.Label lbl_titulo;
         private System.Windows.Forms.Label lbl_mensaje;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_codigo;
     }
 }
--- Clinica/Clinica/FR_CORREO.cs
+++ Clinica/Clinica/FR_CORREO.cs
@@ -10,75 +10,141 @@ using System.Text;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
+using System.Data.Odbc;
+using Clinica.CLS_CLASES;
 
 namespace Clinica
 {
     public partial class FR_CORREO : Form
     {
+        CLS_VALIDACION validacion = new CLS_VALIDACION();
+        Conexion con = new Conexion();
+        string strCorreo;
         public FR_CORREO()
         {
             InitializeComponent();
+            txt_para.Enabled = false;
+            btn_enviar.Enabled = false;
         }
-
-        private void FR_CORREO_Load(object sender, EventArgs e)
+        
+        public void limpiar()
         {
-
+            txt_para.Text = "";
+            txt_asunto.Text = "";
+            txt_mensaje.Text = "";
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-
-        }
-
+        bool bolBanderacorreo;
+        string strPatch;
         private void button2_Click(object sender, EventArgs e)
         {
             //BRIAN SANTIZO
-
-            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
-
-            mmsg.To.Add(txt_para.Text);
-            mmsg.Subject = txt_asunto.Text;
-            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
-            mmsg.Bcc.Add(txt_cc.Text);
-            mmsg.Body = txt_mensaje.Text;
-            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
-            mmsg.IsBodyHtml = true;
-            mmsg.From = new System.Net.Mail.MailAddress("lclinico12@gmail.com");
-
-            //CONEXION CON EL SERVIDOR DE GMAIL
-            //INGRESO DE CREDENCIALES DEL CORREO 
-            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
-            cliente.Credentials = new System.Net.NetworkCredential("lclinico12@gmail.com", "Laboratorio123");
-            cliente.Port = 587;
-            cliente.EnableSsl = true;
-            cliente.Host = "smtp.gmail.com";
-            try
+            if (bolBanderacorreo == true && txt_para.Text != "")
             {
-                //ENVIO DE MENSAJE
-                cliente.Send(mmsg);
-                MessageBox.Show("Correo enviado");
+                System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+                try
+                {
+                    mmsg.To.Add(txt_para.Text);
+                    mmsg.Subject = txt_asunto.Text;
+                    mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+                    mmsg.Body = txt_mensaje.Text;
+                    mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+                    mmsg.IsBodyHtml = true;
+                    mmsg.Attachments.Add(new Attachment(strPatch));
+                    mmsg.From = new System.Net.Mail.MailAddress("lclinico12@gmail.com");
+
+                    //CONEXION CON EL SERVIDOR DE GMAIL
+                    //INGRESO DE CREDENCIALES DEL CORREO 
+                    System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+                    cliente.Credentials = new System.Net.NetworkCredential("lclinico12@gmail.com", "Laboratorio123");
+                    cliente.Port = 587;
+                    cliente.EnableSsl = true;
+                    cliente.Host = "smtp.gmail.com";
+
+                    //ENVIO DE MENSAJE
+                    cliente.Send(mmsg);
+                    MessageBox.Show("Correo enviado");
+                    txt_codigo.Text = "";
+                    limpiar();
+                }
+                catch (Exception)
+                {
+                    limpiar();
+                    //SI NO LO ENVIA, MUESTRA MENSAJE DE ERROR
+                    MessageBox.Show("Error al enviar");
+                }
             }
-            catch (Exception)
-            {
-                //SI NO LO ENVIA, MUESTRA MENSAJE DE ERROR
-                MessageBox.Show("Error al enviar");
-            }
+            
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            this.Hide();
+            this.Close();
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
 
+        private void btn_adjuntar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Examinar = new OpenFileDialog();
+            if(Examinar.ShowDialog() == DialogResult.OK)
+            {
+                strPatch = Examinar.FileName;
+            }
         }
 
-        private void TX_PARA_TextChanged(object sender, EventArgs e)
+        bool boBandera;
+        private void txt_codigo_KeyPress(object sender, KeyPressEventArgs e)
         {
+            validacion.CampoNumerico(e);
+            if (Char.IsNumber(e.KeyChar))
+            {
+                limpiar();
+                boBandera = true;
+            }
+            else
+            {
+                limpiar();
+                boBandera = false;
+            }
+        }
+        //Bryan Mazariegos 
+        //codigo para obtener correo
+        private void txt_codigo_TextChanged_1(object sender, EventArgs e)
+        {
+            if (boBandera == true && txt_codigo.Text != "")
+            {
+                try
+                {
+                    string strSql = "SELECT correo_paciente FROM tbl_paciente WHERE pk_id_paciente = " + txt_codigo.Text + ";";
+                    OdbcCommand command = new OdbcCommand(strSql, con.conexion());
+                    OdbcDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        txt_para.Text = reader[0].ToString();
+                        bolBanderacorreo = true;
+                    }
+                }
+                catch (Exception err)
+                {
+                    bolBanderacorreo = false;
+                    btn_enviar.Enabled = false;
+                    limpiar();
+                    MessageBox.Show(err.Message);
+                }
+            }
+        }
+
+        private void txt_para_TextChanged(object sender, EventArgs e)
+        {
+            if(txt_para.Text=="")
+            {
+                btn_enviar.Enabled = false;
+            }
+            else
+            {
+
+                btn_enviar.Enabled = true;
+            }
 
         }
     }
