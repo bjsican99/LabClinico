53e0e66174542584da8b21378e795e56d7b402b3
bjsican99 <47334369+bjsican99@users.noreply.github.com>
2020-08-19 20:57:20 -0600

    Reportes Actualizados

--- Clinica/Clinica/FR_REPORTES.cs
+++ Clinica/Clinica/FR_REPORTES.cs
@@ -38,5 +38,58 @@ namespace Clinica
             //CODIGO PARA HACER TRASLUCIDO EL PANEL
             pnl_reportes.BackColor = Color.FromArgb(25, 0, 0, 0);
         }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_GANANCIAS gana = new Reportes.FR_GANANCIAS();
+            gana.Show();
+           
+        }
+
+        private void btn_acciones_usuario_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_BITACORA fr = new Reportes.FR_BITACORA();
+            fr.Show();
+            
+        }
+
+        private void btn_examenes_ultima_visita_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_RPACIENTE fr = new Reportes.FR_RPACIENTE();
+            fr.Show();
+            
+        }
+
+        private void btn_resultado_examenes_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_REXAMEN fr = new Reportes.FR_REXAMEN();
+            fr.Show();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_RPACIENTE fr = new Reportes.FR_RPACIENTE();
+            fr.Show();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_UVISITA fr = new Reportes.FR_UVISITA();
+            fr.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_RPAGOS fr = new Reportes.FR_RPAGOS();
+            fr.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reportes.FR_RFACTURA fr = new Reportes.FR_RFACTURA();
+            fr.Show();
+        }
     }
 }
